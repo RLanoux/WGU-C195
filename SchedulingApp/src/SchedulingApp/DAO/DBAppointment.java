94447e321f7ff7cc7b15d41f035502b611c43c8e
Raymond Lanoux <raymondlanoux@gmail.com>
2019-08-05 13:10:23 -0500

    Updated Time Variables

--- SchedulingApp/src/SchedulingApp/DAO/DBAppointment.java
+++ SchedulingApp/src/SchedulingApp/DAO/DBAppointment.java
@@ -1,9 +1,45 @@
 package SchedulingApp.DAO;
 
+import static SchedulingApp.DAO.DBConnector.DB_CONN;
+import SchedulingApp.Model.Appointment;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author Raymond Lanoux <rlanoux@wgu.edu>
  */
 public class DBAppointment {
+    
+    public static ObservableList<Appointment> getApptsByWeek() {
+        ObservableList<Appointment> apptsByWeek = FXCollections.observableArrayList();
+        String getApptsByWeekSQL = "SELECT * FROM appointment WHERE start = ?";
+        
+        try {
+            PreparedStatement stmt = DB_CONN.prepareStatement(getApptsByWeekSQL);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Appointment apptByWeek = new Appointment();
+                apptByWeek.setAppointmentId(rs.getInt("appointmentId"));
+                apptByWeek.setCustomerId(rs.getInt("customerId"));
+                apptByWeek.setUserId(rs.getInt("userId"));
+                apptByWeek.setTitle(rs.getString("title"));
+                apptByWeek.setDescription(rs.getString("description"));
+                apptByWeek.setLocation(rs.getString("location"));
+                apptByWeek.setContact(rs.getString("contact"));
+                apptByWeek.setType(rs.getString("type"));
+                apptByWeek.setUrl(rs.getString("url"));
+                apptByWeek.setStart(rs.getTimestamp("start"));
+                apptByWeek.setEnd(rs.getTimestamp("end"));
+            }
+        }
+        catch (SQLException e) {
+            e.getMessage();
+        }
+    }
     
 }
--- SchedulingApp/src/SchedulingApp/Model/Appointment.java
+++ SchedulingApp/src/SchedulingApp/Model/Appointment.java
@@ -5,6 +5,8 @@
  */
 package SchedulingApp.Model;
 
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 
 /**
@@ -21,30 +23,14 @@ public class Appointment {
     private String contact;
     private String type;
     private String url;
-    private Calendar start;
-    private Calendar end;
-    private Calendar createDate;
+    private ZonedDateTime start;
+    private ZonedDateTime end;
+    private ZonedDateTime createDate;
     private String createdBy;
-    private Calendar lastUpdate;
+    private ZonedDateTime lastUpdate;
     private String lastUpdateBy;
     
-    public Appointment(int appointmentId, int customerId, int userId, String title, String description, String location, String contact, String type, String url,
-                            Calendar start, Calendar end, Calendar createDate, String createdBy, Calendar lastUpdate, String lastUpdateBy) {
-        this.appointmentId = appointmentId;
-        this.customerId = customerId;
-        this.userId = userId;
-        this.title = title;
-        this.description =  description;
-        this.location = location;
-        this.contact = contact;
-        this.type = type;
-        this.url = url;
-        this.start = start;
-        this.end = end;
-        this.createDate = createDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdateBy = lastUpdateBy;
+    public Appointment() {
     }
 
     public int getAppointmentId() {
@@ -119,19 +105,19 @@ public class Appointment {
         this.url = url;
     }
 
-    public Calendar getStart() {
+    public Timestamp getStart() {
         return start;
     }
 
-    public void setStart(Calendar start) {
+    public void setStart(Timestamp start) {
         this.start = start;
     }
 
-    public Calendar getEnd() {
+    public Timestamp getEnd() {
         return end;
     }
 
-    public void setEnd(Calendar end) {
+    public void setEnd(Timestamp end) {
         this.end = end;
     }
 
