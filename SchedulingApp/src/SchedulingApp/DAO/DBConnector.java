2eb493e99229af432ff5083eabe77a4ad99e239d
Raymond Lanoux <raymondlanoux@gmail.com>
2019-09-19 18:43:33 -0500

    Update DBConnector

--- SchedulingApp/src/SchedulingApp/DAO/DBConnector.java
+++ SchedulingApp/src/SchedulingApp/DAO/DBConnector.java
@@ -17,11 +17,6 @@ public class DBConnector {
     private static final String USERNAME = "U04PjR";
     private static final String PASSWORD = "53688305516";
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    
-    /*private static final String DB_NAME = "c195test";
-    private static final String DB_URL = "jdbc:mysql://127.0.0.1/"+DB_NAME;
-    private static final String USERNAME = "rlanoux";
-    private static final String PASSWORD = "P@ss2019!";*/
             
     static Connection DB_CONN;
     
